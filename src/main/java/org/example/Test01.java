77444f8746e8c5322af577e8e583afab92401764
Chuhao <1483791990@qq.com>
2026-03-11 19:51:50 +0800

    项目初始化

--- src/main/java/org/example/Test01.java
+++ src/main/java/org/example/Test01.java
@@ -1,6 +1,6 @@
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello test01");
-        System.out.println("master control this txt");
+        System.out.println("master control this txt2");
     }
 }
